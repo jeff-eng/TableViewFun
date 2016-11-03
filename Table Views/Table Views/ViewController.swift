89c782abfe209fb305791157ad382e7fb1c74888
Jeff Eng <jeff@jeffeng.me>
2016-11-02 17:07:03 -0700

    created table view cell and added content to table view cell

---
+++ Table Views/Table Views/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Table Views
+//
+//  Created by Jeffrey Eng on 11/2/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+    var cellContent = ["Rob", "Kirsten", "Tommy", "Ralphie"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
