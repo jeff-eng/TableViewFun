088ae81bcf58ad787741e78dea4afeb361c4453c
Jeff Eng <jeff@jeffeng.me>
2016-11-16 11:38:52 -0800

    created a new UIView subclass and implemented an initializer

---
+++ Food Tracker/Food Tracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  Food Tracker
+//
+//  Created by Jeffrey Eng on 11/16/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
