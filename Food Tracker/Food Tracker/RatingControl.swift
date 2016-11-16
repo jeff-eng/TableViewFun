2c61a5a389faf5dc8b52cfa6f95f1d352a68ae7a
Jeff Eng <jeff@jeffeng.me>
2016-11-16 14:45:19 -0800

    added rating property and ratingButton array to keep track of times button tapped

--- Food Tracker/Food Tracker/RatingControl.swift
+++ Food Tracker/Food Tracker/RatingControl.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class RatingControl: UIView {
 
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        for _ in 0..<5 {
+        // Create a red button
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+        ratingButtons += [button]
+        // Adds the button created above to the RatingControl view
+        addSubview(button)
+        }
     }
     
+    override public var intrinsicContentSize: CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
 }
