163f3af1bde31384f8c49f477358d5715174d761
Jeff Eng <jeff@jeffeng.me>
2016-11-16 16:39:28 -0800

    added star images to ratings and the states when buttons selected

--- Food Tracker/Food Tracker/RatingControl.swift
+++ Food Tracker/Food Tracker/RatingControl.swift
@@ -14,15 +14,23 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    // Set spacing value between each star
+    let spacing = 5
+    let starCount = 5
     
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
+        
+        for _ in 0..<starCount {
         // Create a red button
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.red
+        let button = UIButton()
+        button.setImage(emptyStarImage, for: .normal)
+        button.setImage(filledStarImage, for: .selected)
+        button.setImage(filledStarImage, for: [.highlighted, .selected])
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
         // Add button to the array
         ratingButtons += [button]
@@ -32,18 +40,24 @@ class RatingControl: UIView {
     }
     
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             button.frame = buttonFrame
         }
         
     }
     
     override public var intrinsicContentSize: CGSize {
-        return CGSize(width: 240, height: 44)
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
     }
     
     // MARK: Button Action
