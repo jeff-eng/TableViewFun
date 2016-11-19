d18dd6a9b421bab19551adace680742729563825
Jeff Eng <jeff@jeffeng.me>
2016-11-18 16:08:25 -0800

    created the table view controller. also created a custom mealtableviewcell class along with a custom table view cell

---
+++ Food Tracker/Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by Jeffrey Eng on 11/18/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
