e0ffdb7bae7a3f35a8d5563e1a8ab94271ac632f
Jeff Eng <jeff@jeffeng.me>
2016-12-03 17:18:56 -0800

    Created file path to data by accessing the file manager and creating a URL. Ran build and no issues.

--- Food Tracker/Food Tracker/Meal.swift
+++ Food Tracker/Food Tracker/Meal.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
     
     // MARK: Initialization
     
@@ -23,9 +37,31 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         // Initialization should fail if there is no name or if the rating is negative
         if name.isEmpty || rating < 0 {
             return nil
         }
+    }
+    
+    // MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.nameKey)
+        aCoder.encode(photo, forKey: PropertyKey.photoKey)
+        aCoder.encode(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        // Unarchives the stored information about an object.
+        let name = aDecoder.decodeObject(forKey: PropertyKey.nameKey) as! String
+        // Downcast as UIImage assigned to photo constant. Use optional type cast because it is possible the value could be nil (no photo present)
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photoKey) as? UIImage
+        // Unarchives an integer.
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.ratingKey)
+        
+        // Must call designated initializer
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
